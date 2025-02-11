15f11f8aaf0302d57d599cfe9160b755e9e70edd
Damchan <damchan113@gmail.com>
2025-02-11 23:14:28 +0900

    [fea] Changing the Directory Structure and add flowModule
    - add flowModule for command routing
    
    - make Modules

--- UserModuele/user.h
+++ UserModuele/user.h
@@ -2,10 +2,13 @@
 #ifdef USER_H
 #include <ATMModule.h>
 #include <accounrControl.h>
+#include <pthread.h>
 #include <type.h>
+#include <unistd.h>
 
 #include <fstream>
 #include <iostream>
+#include <thread>
 
 class UserInfo
 {
@@ -14,7 +17,10 @@ class UserInfo
     uint64_t pinNum;
     bool isPinverified;
     std::vector<Account> accounts;
-    ATMStatus atmStatus;
+
+    std::thread userThread;
+
+    void processUserInput();  // 사용자 입력을 처리하는 스레드 함수
 
    public:
     UserInfo() : cardNumber(""), pinNum(0) {}
