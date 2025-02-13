43ff478cfc3854ab75db0e539a228322b772244f
Damchan <damchan113@gmail.com>
2025-02-13 23:32:39 +0900

    [ref]Refactor command processing in FlowModule to handle ATM_CHECK_BALANCE and improve command structure

--- FlowModule/flowModule.cpp
+++ FlowModule/flowModule.cpp
@@ -38,7 +38,7 @@ void FlowModule::processCommands()
             commandQueue.push(cmd);
         }
         Command newCmd;
-        if (cmd.TX == "User")
+        if (cmd.RX == "FlowModule")
         {
             {
                 switch (cmd.type)
@@ -60,10 +60,8 @@ void FlowModule::processCommands()
                         std::cout << "[FlowModule] Verifying PIN for " << cmd.cardNumber
                                   << std::endl;
                         newCmd = {
-                            cmd.type,    "FlowModule", "ATM", cmd.cardNumber, cmd.pin, 0,
-                            0,
-                            cmd.callback  // ✅ 올바르게 전달됨을 보장
-                        };
+                            cmd.type, "FlowModule", "ATM", cmd.cardNumber, cmd.pin, 0,
+                            0,        cmd.callback};
 
                         if (cmd.callback == nullptr)
                         {
@@ -100,38 +98,23 @@ void FlowModule::processCommands()
                         commandQueue.push(newCmd);
                         break;
 
+                    case CommandType::ATM_CHECK_BALANCE:
+                        std::cout << "[FlowModule] Checking balance for "
+                                  << cmd.cardNumber << std::endl;
+                        newCmd = {CommandType::ATM_CHECK_BALANCE,
+                                  "FlowModule",
+                                  "ATM",
+                                  cmd.cardNumber,
+                                  0,
+                                  0,
+                                  NULL,
+                                  cmd.callback};
+                        commandQueue.push(newCmd);
+                        break;
                     default:
                         break;
                 }
             }
-
-            // 2. Process ATM → BankAPI commands
-            // if (cmd.TX == "ATM")
-            // {
-            //     switch (cmd.type)
-            //     {
-            //         case CommandType::ATM_DEPOSIT:
-            //             BankAPI::deposit(cmd.cardNumber, cmd.amount);
-            //             double balance = BankAPI::getBalance(cmd.cardNumber);
-            //             std::cout << "[FlowModule] New balance: $" << balance
-            //                       << std::endl;
-            //             break;
-            //         case CommandType::ATM_WITHDRAW:
-            //             BankAPI::withdraw(cmd.cardNumber, cmd.amount);
-            //             double balance = BankAPI::getBalance(cmd.cardNumber);
-            //             std::cout << "[FlowModule] New balance: $" << balance
-            //                       << std::endl;
-            //             break;
-            //         case CommandType::ATM_CHECK_BALANCE:
-            //             BankAPI::getBalance(cmd.cardNumber);
-            //             break;
-            //         case CommandType::ATM_LOAD_ACCOUNT_INFO:
-            //             BankAPI::loadAccounts();
-            //             break;
-            //         default:
-            //             break;
-            //     }
-            // }
         }
     }
 }
